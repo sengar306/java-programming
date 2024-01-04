f0b47f18a5f6daa6a271d023b9ba7a31eaf42d9d
sengar306 <viveksengar921927@gmail.com>
2024-01-04 12:17:21 +0530

    inheritance

---
+++ infinite.java
@@ -0,0 +1,19 @@
+public class infinite {
+    public static void main(String args[])
+    {
+        int n =5;
+        while(n<=201);
+        {  
+             System.out.print(","+n);
+            n=n+4;
+        }
+        // for(int i=0;i<=40;i++);
+
+       //{ System.out.println(n); 
+
+
+       }   
+        
+    }
+
+}
---
+++ inheritance.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class inheritance{
+ public static void main(String[] args) {
+      Numericalcalculation s=new  Numericalcalculation();    
+      s.addition();   
+ }
+}
+class Number{
+    protected int num1,num2;
+    //constructor to intialize data through input
+    public Number(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter any two number");
+         num1=sc.nextInt();
+         num2=sc.nextInt();
+
+    }
+    public void display()
+    {
+        System.out.println("__---------------------------------------------");
+        System.out.println("firstno" + num1);
+         System.out.println("secondno" + num2);
+    }
+}
+class Numericalcalculation extends Number{
+//method for addition of number
+public void addition()
+{
+    int sum;
+    sum=super.num1+super.num2;
+    //display numbers
+    super.display();
+    System.out.println("sum = "+ sum);
+}
+public void difference()
+    {
+        int sub=super.num1-super.num2;
+        System.out.println("difference between " +super.num1 +"and" +super.num2 );
+
+    }
+}
