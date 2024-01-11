430f459b0ff6e5ab9e347afcc15ec7de10b42552
sengar306 <viveksengar921927@gmail.com>
2024-01-11 12:54:12 +0530

    exception handling

---
+++ ASSIGNMENT/divide/finally.java
@@ -0,0 +1,19 @@
+public class finally{
+    public static void main(String[] args) {
+        try {
+            // Code that may throw an exception
+            int result = divide(10, 0);
+            System.out.println("Result: " + result); // This line won't be executed
+        } catch (ArithmeticException e) {
+            // Code to handle the exception
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            // Code that will be executed no matter what
+            System.out.println("Finally block executed.");
+        }
+    }
+
+    static int divide(int a, int b) {
+        return a / b;
+    }
+}
---
+++ ASSIGNMENT/divide/index.java
@@ -0,0 +1,14 @@
+import  java.util.*;
+class index{
+    public static void main (String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        float num1 = sc.nextInt();
+        float num2=sc.nextInt();
+        num1=num2/num1;
+        System.out.println(num1);
+
+
+    }
+
+}
